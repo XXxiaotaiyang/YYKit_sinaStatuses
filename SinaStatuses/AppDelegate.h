19896795c6b0ab74e42a188f45c51fb6bb9c6475
张超超 <976325486@.com>
2015-12-14 15:01:00 +0800

    初始化项目

---
+++ SinaStatuses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaStatuses
+//
+//  Created by 闲人 on 15/12/14.
+//  Copyright © 2015年 闲人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinaStatuses/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SinaStatuses
+//
+//  Created by 闲人 on 15/12/14.
+//  Copyright © 2015年 闲人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
